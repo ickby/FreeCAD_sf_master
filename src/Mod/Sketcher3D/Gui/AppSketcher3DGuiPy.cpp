2ca6a943d440f7f2e74c28b8679e5ffc17a5ad47
Stefan Tröger <stefantroger@gmx.de>
2013-02-11 12:20:00 +0100

    update copytight

--- src/Mod/Sketcher3D/Gui/AppSketcher3DGuiPy.cpp
+++ src/Mod/Sketcher3D/Gui/AppSketcher3DGuiPy.cpp
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (c) YEAR YOUR NAME         <Your e-mail address>            *
+ *   Copyright (c) Stefan Tröger        <stefantroeger@gmx.net>            *
  *                                                                         *
  *   This file is part of the FreeCAD CAx development system.              *
  *                                                                         *
