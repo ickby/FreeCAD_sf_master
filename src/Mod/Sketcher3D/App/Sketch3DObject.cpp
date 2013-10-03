7e395359eb87d5c2e799cbb3459247c71ac5abf5
Stefan Tröger <stefantroeger@gmx.net>
2013-10-03 18:41:14 +0000

    add sketch3d object

---
+++ src/Mod/Sketcher3D/App/Sketch3DObject.cpp
@@ -0,0 +1,87 @@
+/***************************************************************************
+ *   Copyright (c) Stefan Tröger         (stefantroeger@gmx.net) 2013      *
+ *                                                                         *
+ *   This file is part of the FreeCAD CAx development system.              *
+ *                                                                         *
+ *   This library is free software; you can redistribute it and/or         *
+ *   modify it under the terms of the GNU Library General Public           *
+ *   License as published by the Free Software Foundation; either          *
+ *   version 2 of the License, or (at your option) any later version.      *
+ *                                                                         *
+ *   This library  is distributed in the hope that it will be useful,      *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU Library General Public License for more details.                  *
+ *                                                                         *
+ *   You should have received a copy of the GNU Library General Public     *
+ *   License along with this library; see the file COPYING.LIB. If not,    *
+ *   write to the Free Software Foundation, Inc., 59 Temple Place,         *
+ *   Suite 330, Boston, MA  02111-1307, USA                                *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "PreCompiled.h"
+#include "Sketch3DObject.h"
+#include <Base/Console.h>
+#include "Sketch3DObjectPy.h"
+
+using namespace Sketcher3D;
+
+
+PROPERTY_SOURCE(Sketcher3D::Sketch3DObject, Part::Feature)
+
+Sketch3DObject::Sketch3DObject() : Part::Feature(), m_geom_idx(0), m_cons_idx(0)  {
+  
+      ADD_PROPERTY_TYPE(m_solver, (0)  ,"Sketch3D",(App::PropertyType)(App::Prop_None),"three dimensional sketch solver");
+};
+
+Sketch3DObject::~Sketch3DObject() {};
+
+
+int Sketch3DObject::addGeometry(const Part::Geometry* geo) {
+
+    m_solver.initSetValue();
+    
+    Part::Geometry* geoNew = geo->clone();
+    m_geom_idx++;
+    
+    if (geo->getTypeId() == Part::GeomPoint::getClassTypeId()) {
+      Part::GeomPoint* p = dynamic_cast<Part::GeomPoint*>(geoNew);
+      m_solver.createGeometry3D(p, m_geom_idx);
+    }
+    
+    m_solver.finishSetValue();
+    
+    return m_geom_idx;
+};
+
+void Sketch3DObject::delGeometry(int id) {
+  
+    m_solver.initSetValue();
+    m_solver.removeGeometry3D(id);
+    m_solver.finishSetValue();
+}
+
+
+PyObject* Sketch3DObject::getPyObject(void) {
+    if(PythonObject.is(Py::_None())) {
+        // ref counter is set to 1
+        PythonObject = Py::Object(new Sketch3DObjectPy(this),true);
+    }
+    return Py::new_reference_to(PythonObject);
+}
+
+// Python Sketcher feature ---------------------------------------------------------
+
+namespace App {
+/// @cond DOXERR
+PROPERTY_SOURCE_TEMPLATE(Sketcher3D::Sketch3DObjectPython, Sketcher3D::Sketch3DObject)
+template<> const char* Sketcher3D::Sketch3DObjectPython::getViewProviderName(void) const {
+    return "Sketcher3D::ViewProviderPython";
+}
+/// @endcond
+
+// explicit template instantiation
+template class Sketcher3DExport FeaturePythonT<Sketcher3D::Sketch3DObject>;
+}
+
---
+++ src/Mod/Sketcher3D/App/Sketch3DObject.h
@@ -0,0 +1,67 @@
+/***************************************************************************
+ *   Copyright (c) Stefan Tröger         (stefantroeger@gmx.net) 2013      *
+ *                                                                         *
+ *   This file is part of the FreeCAD CAx development system.              *
+ *                                                                         *
+ *   This library is free software; you can redistribute it and/or         *
+ *   modify it under the terms of the GNU Library General Public           *
+ *   License as published by the Free Software Foundation; either          *
+ *   version 2 of the License, or (at your option) any later version.      *
+ *                                                                         *
+ *   This library  is distributed in the hope that it will be useful,      *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU Library General Public License for more details.                  *
+ *                                                                         *
+ *   You should have received a copy of the GNU Library General Public     *
+ *   License along with this library; see the file COPYING.LIB. If not,    *
+ *   write to the Free Software Foundation, Inc., 59 Temple Place,         *
+ *   Suite 330, Boston, MA  02111-1307, USA                                *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef SKETCHER3D_SKETCH3DOBJECT_H
+#define SKETCHER3D_SKETCH3DOBJECT_H
+
+#include <Part/App/PartFeature.h>
+#include <Part/App/PropertyGeometryList.h>
+
+#include "Solver.hpp"
+
+
+namespace Sketcher3D {
+
+class Sketcher3DExport Sketch3DObject : public Part::Feature {
+
+    PROPERTY_HEADER(Sketcher3D::Sketch3DObject);
+    
+    Solver System;
+
+public:
+
+    Sketch3DObject();
+    virtual ~Sketch3DObject();
+
+    //Property
+    Solver   m_solver;
+    
+    //geometry and constraint numbering counters
+    int m_geom_idx, m_cons_idx;
+    
+
+    int addGeometry(const Part::Geometry* geo);
+    
+    // returns the type name of the ViewProvider
+    const char* getViewProviderName(void) const {
+        return "Sketcher3DGui::ViewProviderSketch3D";
+    }
+    
+    // from base class
+    virtual PyObject *getPyObject(void);
+};
+
+typedef App::FeaturePythonT<Sketch3DObject> Sketch3DObjectPython;
+
+}
+
+#endif // SKETCHER3D_SKETCH3DOBJECT_H
